0ef2c69b4ce672fa75705f3304784a9bc9645cfe
Tanapat <a_little_kids@hotmail.com>
2024-10-21 20:41:22 +0700

    another example

--- src/Tanapat.FpPractices/Program.cs
+++ src/Tanapat.FpPractices/Program.cs
@@ -2,11 +2,24 @@
 
 var sample = "0002010102112632002816728000581200000000100000055204581253031445502015802LK5909Vits Food6007Colombo61050080062580032537c0a88562e4a599cab63d1992f0dac05181600766683296-000563042AB7";
 
-CutQr(sample)
+var result = CutQr(sample)
     .Bind(BuildHtmlOutput)
     .Match(
-        Succ: (output) => File.WriteAllText("output.html", output),
-        Fail: (ex) => Console.WriteLine("error: {0}", ex.Message));
+        Succ: HandleSuccess,
+        Fail: HandleFail);
+
+Console.WriteLine(result);
 
 Console.WriteLine("Press any key to continue");
 Console.Read();
+
+static string HandleSuccess(string output)
+{
+    File.WriteAllText("output.html", output);
+    return "success";
+}
+
+static string HandleFail(Exception ex)
+{
+    return $"error: {ex.Message}";
+}
