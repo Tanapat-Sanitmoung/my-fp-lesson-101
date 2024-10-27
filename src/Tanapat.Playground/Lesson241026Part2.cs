6f0417adf6cedba46d5036c16df8a58901409ecf
Tanapat <a_little_kids@hotmail.com>
2024-10-27 08:03:41 +0700

    clean code

--- src/Tanapat.Playground/Lesson241026Part2.cs
+++ src/Tanapat.Playground/Lesson241026Part2.cs
@@ -15,52 +15,62 @@ public static class Lesson241026Part2
                 .Map(ctx => ctx.Success)
                 .IfFail(f => false);
     static Try<ExecutionContext> CreateExecutionContext(string[] folders, string[] patterns)
-        => Try(() => 
-            new ExecutionContext(
-                Folders: folders.Map(f => new Folder(f)).ToSeq(),
-                SearchPatterns: patterns.Map(p => new SearchPattern(p)).ToSeq()));
+        => Try(() =>
+                new ExecutionContext(
+                    Folders: GetFolders(folders),
+                    SearchPatterns: GetSearchPatterns(patterns)
+                )
+            );
+
+    static Seq<Folder> GetFolders(string[] folders)
+        => folders
+            .Map(f => new Folder(f))
+            .ToSeq();
+
+    static Seq<SearchPattern> GetSearchPatterns(string[] searchPatterns)
+        => searchPatterns
+            .Map(p => new SearchPattern(p))
+            .ToSeq();
 
     static Try<ExecutionContext> GetFiles(ExecutionContext context)
-        => Try(() => 
-            context with 
+        => Try(() =>
+            context with
             {
-                Files = context
-                    .Folders
-                    .Fold(
-                        state: Seq<File>(), 
-                        (s, fd) => s.Append(Mock(fd, context.SearchPatterns)))
+                Files = GetFilesInFolders(context.Folders, context.SearchPatterns)
             });
 
-    static Seq<File> Mock(Folder fd, Seq<SearchPattern> patterns)
+    static Seq<File> GetFilesInFolders(Seq<Folder> folders, Seq<SearchPattern> patterns)
+        => folders
+            .Fold(
+                state: Seq<File>(),
+                (state, eachFolder) => state.Append(MockSearchFiles(eachFolder, patterns)));
+
+    static Seq<File> MockSearchFiles(Folder fd, Seq<SearchPattern> patterns)
         => patterns.Map(
             p => new File($"{fd.Name} {p.Value}"));
 
     static Try<ExecutionContext> PrintFilesContent(ExecutionContext context)
-        => Try(() => {
+        => Try(() =>
+        {
             context.Files
                 .Iter(f => Console.WriteLine($"Content of {f.Name}"));
-            
-            return context with {
+
+            return context with
+            {
                 Success = true
             };
         });
 
     public record ExecutionContext(Seq<Folder> Folders, Seq<SearchPattern> SearchPatterns)
     {
-        public Seq<File> Files { get; set;}
-    public bool Success { get; internal set; }
-}
+        public Seq<File> Files { get; set; }
+        public bool Success { get; internal set; }
+    }
 
     public record Folder(string Name);
 
     public record File(string Name);
 
     public record SearchPattern(string Value);
-
-}
-
-
-public interface IFileService
-{
 
 }
