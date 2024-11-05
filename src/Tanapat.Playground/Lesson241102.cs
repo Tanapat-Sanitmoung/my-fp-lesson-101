abb4503016e2fa616cc365351a7a5737c5ee4020
Tanapat <a_little_kids@hotmail.com>
2024-11-05 22:53:24 +0700

    try to make it more declarative

--- src/Tanapat.Playground/Lesson241102.cs
+++ src/Tanapat.Playground/Lesson241102.cs
@@ -1,4 +1,6 @@
 
+using LanguageExt.SomeHelp;
+
 namespace Tanapat.Playground;
 
 public static class Lesson241102
@@ -7,18 +9,47 @@ public static class Lesson241102
     {
         string rawString = "0002020102111501A6001B63045567";
 
-        var reader = new StrReader(rawString);
-
-        while (reader.NotAtEnd())
-        {
-            var str1 = reader.Read(2);
-            var str2 = reader.Read(2);
-            var str3 = reader.Read(ushort.Parse(str2));
-
-            Console.WriteLine($"{str1,-4}{str3,-50}");
-        }
-
+        _ = QrReader.Convert(rawString)
+            .Match(
+                Right: (r) => r.Iter(b => Console.WriteLine($"{b.Id}  {b.Value}")), 
+                Left : (l) => { 
+                        Console.WriteLine(l.Message); 
+                        return unit;
+                    }
+            );
     }
+
+    class QrReader 
+    {
+        public static Either<Problem, Seq<Block>> Convert(string rawString)
+        {
+            try
+            {
+                var output = new List<Block>();
+
+                var reader = new StrReader(rawString);
+
+                while (reader.NotAtEnd())
+                {
+                    var str1 = reader.Read(2);
+                    var str2 = reader.Read(2);
+                    var str3 = reader.Read(ushort.Parse(str2));
+
+                    output.Add(new(str1, str3));
+                }
+
+                throw new Exception("Test");
+                return output.ToSeq();
+            }
+            catch (Exception ex)
+            {
+                return new Problem(ex.Message);
+            }
+        }
+    }
+
+    record Block(string Id, string Value);
+    record Problem(string Message);
 
     class StrReader
     {
