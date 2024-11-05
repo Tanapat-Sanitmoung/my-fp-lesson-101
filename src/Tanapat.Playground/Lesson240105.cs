7d461be774fc7638667f0bb587357a977ccb2944
Tanapat <a_little_kids@hotmail.com>
2024-11-05 23:30:50 +0700

    just another idea

---
+++ src/Tanapat.Playground/Lesson240105.cs
@@ -0,0 +1,61 @@
+namespace Tanapat.Playground;
+
+public class Lesson241105
+{
+    public static void Execute()
+    {
+        var rawString = "0002020102111501A6001B63045567";
+
+        new BlockReader()
+            .Read(rawString, (id, value) => new Block(id, value))
+            .Match(
+                Succ: s => s.Iter(b => Console.WriteLine($"{b.Id} {b.Value}")),
+                Fail: ex => Console.WriteLine("Problem : {0}", ex.Message)
+            );
+    }
+
+    record Block(string Id, string Value);
+
+    class BlockReader 
+    {
+        private int _index;
+        private string _source = string.Empty;
+
+        private string Read(int len) 
+            => (_source.Substring(_index,  len), _index+= len).Item1;
+
+        private string ReadId()
+            => Read(2);
+
+        private int ReadValueLen()
+            => int.Parse(Read(2));
+
+        private string ReadValue()
+            => Read(ReadValueLen());
+
+        private (string Id, string Value) ReadBlock()
+            => (ReadId(), ReadValue());
+
+        private bool NotEnd()
+            => _index < _source.Length;
+
+        private void Init(string source)
+            => (_source, _index) = (source, 0) ;
+
+        public Try<Seq<T>> Read<T>(string source, Func<string, string, T> convert)
+            => Try(() => 
+            {
+                Init(source);
+
+                var appender = new List<T>();
+
+                while (NotEnd())
+                {
+                    var (id, value) = ReadBlock();
+                    appender.Add(convert(id, value));
+                }
+
+                return appender.ToSeq();
+            });
+    }
+}
