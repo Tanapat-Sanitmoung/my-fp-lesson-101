6bbde6092a060abe6394ec3f100a273ce2e0d297
Tanapat <a_little_kids@hotmail.com>
2024-11-09 23:12:40 +0700

    overload

--- src/Tanapat.Playground.FSharp/241109.fsx
+++ src/Tanapat.Playground.FSharp/241109.fsx
@@ -89,3 +89,71 @@ f1.increaseAgeByOne
 f1.decreaseAgeByOne
 f1.printAge
 f2.increaseAgeByOne
+
+type Faa() =
+  let mutable num = 0
+
+  member me.count = 
+    num <- num + 1 
+    printfn "Count :%d" num
+
+let fa1 = Faa()
+fa1.count
+
+let fa2 = Faa()
+fa2.count
+fa2.count
+
+let fa3 = new Faa()
+fa3.count
+
+type MyType(?x: int, ?y: int) =
+    member val X = defaultArg x 0 with get, set
+    member val Y = defaultArg y 0 with get, set
+
+let instance1 = MyType()        // Calls constructor with no arguments
+let instance2 = new MyType(5)   // Calls constructor with one argument
+let instance3 = new MyType(5, 10) // Calls constructor with two arguments
+let instance4 = MyType(5)
+let instance5 = MyType(5, 10)
+
+printfn "Instance1: X=%d, Y=%d" instance1.X instance1.Y
+printfn "Instance2: X=%d, Y=%d" instance2.X instance2.Y
+printfn "Instance3: X=%d, Y=%d" instance3.X instance3.Y
+printfn "Instance4: X=%d, Y=%d" instance4.X instance4.Y
+printfn "Instance5: X=%d, Y=%d" instance5.X instance5.Y
+
+type MyClass (v: int) =
+  // Default constructor
+  new() = 
+    MyClass(0)
+
+  member this.Value = v
+
+let my1 = MyClass()       // Calls default constructor
+let my2 = new MyClass(5)  // Calls constructor with integer parameter
+
+printfn "Instance1 Value: %d" my1.Value
+printfn "Instance2 Value: %d" my2.Value
+
+
+type MyClassV2() =
+  // Private mutable field to hold the value
+  let mutable value = 0
+
+  // Secondary constructor with an integer parameter
+  new(v: int) as me =
+      MyClassV2()
+      then
+        me.SetValue v
+        printfn "Constructing with value: %d" v
+  member private me.SetValue v=
+    value <- v
+
+  member me.Value = value
+
+let mv21 = MyClassV2()
+let mv22 = MyClassV2(2)
+
+printfn "Instance1 Value: %d" mv21.Value
+printfn "Instance2 Value: %d" mv22.Value
