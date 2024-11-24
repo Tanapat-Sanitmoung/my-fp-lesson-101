5b6fa06335bb5f869eb547e010fdd46b9fab526d
Tanapat <a_little_kids@hotmail.com>
2024-11-24 22:22:58 +0700

    practice

--- src/Tanapat.Playground/Lesson241124.cs
+++ src/Tanapat.Playground/Lesson241124.cs
@@ -22,4 +22,30 @@ public static class Playing
         // LastName-XMiddle-YTanapat
         // LastName-XMiddle-YLawan
     }
+
+    public static void Play02()
+    {
+        // Func<string, string, string, string>
+        var FormatName = (string last, string mid, string first) => $"{last} {mid} {first}";
+
+        var SetFirstName = FormatName
+                .Apply("LastName-Z")
+                .Apply("Middle-Y");
+
+        Console.WriteLine(SetFirstName("Tanapat"));
+        Console.WriteLine(SetFirstName("Lawan"));
+
+        // LastName-Z Middle-Y Tanapat
+        // LastName-Z Middle-Y Lawan
+    }
+
+}
+
+public static class FP
+{
+    public static Func<T2, T3, R> Apply<T1, T2, T3, R>(this Func<T1, T2, T3, R> @this, T1 t1)
+        => (t2, t3) => @this(t1, t2, t3);
+
+    public static Func<T2, R> Apply<T1, T2, R>(this Func<T1, T2, R> @this, T1 t1)
+        => t2 => @this(t1, t2);
 }
