9db88a7d3645f1fc84d3d534dfae124bb88a9dfe
Tanapat <a_little_kids@hotmail.com>
2024-11-30 20:44:01 +0700

    practice

--- src/Tanapat.Playground/Lesson241128.cs
+++ src/Tanapat.Playground/Lesson241128.cs
@@ -1,5 +1,6 @@
 using LanguageExt;
 using LanguageExt.Common;
+using LanguageExt.Pipes;
 using LanguageExt.SomeHelp;
 using static LanguageExt.Prelude;
 
@@ -12,7 +13,7 @@ public class Program
         var computation =
             from a in Some(100)
             from b in MoreThan50(a)
-            from _1 in WriteOutput(b)
+            from _1 in WriteOutputOption(b)
             select _1;
 
         Console.WriteLine("MainX done");
@@ -26,19 +27,51 @@ public class Program
         computation2.Run(); 
         
         Console.WriteLine("MainX done 2");
+
+        var computation3 =
+            from a in ValidateLength("Hello, 1234567890, 1234567890, 1234567890, 1234567890, 1234567890")
+            from b in WriteOutputValidation(a)
+            select b;
+
+        computation3.IfFail(err =>  { Console.WriteLine(err.First().Message); return unit; });
+
+        Console.WriteLine("MainX done 3");
+
+         var computation4 =
+            from a in ValidateLength("Hello, 1234567890")
+            from b in WriteOutputValidation(a)
+            select b;
+
+        computation4.IfFail(err =>  { Console.WriteLine(err.First().Message); return unit; });
+
+        Console.WriteLine("MainX done 4");
     }
+
+    public static Validation<Error, string> ValidateLength(string message)
+        => message switch 
+        {
+            { Length: > 50 } => Fail<Error, string>(Error.New("Exceed 50 charactors")),
+            var e when string.IsNullOrWhiteSpace(e) => Fail<Error, string>(Error.New("message must not be null or empty")),
+            _ => Success<Error, string>(message)
+        };
 
     public static Option<int> MoreThan50(int value) 
         => value > 50 ? value : None;
 
     public static Eff<Unit> WriteOutputEff(int value)
+        => WriteOutputEff($"Value is {value}");   
+
+    public static Eff<Unit> WriteOutputEff(string message)
     {
-        Console.WriteLine($"Value is {value}");   
+        Console.WriteLine(message);   
         return SuccessEff(unit);
     }
 
-    public static Option<Unit> WriteOutput(int value)
+    public static Option<Unit> WriteOutputOption(int value)
         =>  WriteOutputEff(value).AsOption();
+
+    public static Validation<Error, Unit> WriteOutputValidation(string message)
+        => WriteOutputEff(message).AsValidation();
 }
 
 public record Person();
@@ -57,4 +90,12 @@ public static class Extensions
         => ma.Match(
             a => SuccessEff(a), 
             () => FailEff<A>(Error.New("None")));
+
+    public static Validation<Error, A> AsValidation<A>(this Eff<A> ma)
+        => ma
+            .Run()
+            .Match(
+                a   => Success<Error, A>(a), 
+                err => Fail<Error, A>(err)
+            );
 }
